d955c5dd9ebbf063e588e68a5aedbf88d16e9ceb
David Ed Mellum <david@edmellum.com>
2012-06-11 10:56:53 +0200

    Typos.

--- a2gchecker.py
+++ a2gchecker.py
@@ -5,8 +5,8 @@ Author: David Ed Mellum <david@edmellum.com> (http://edmellum.com)
 License: MIT
 Checks a list of websites to see if they are up.
 If one is down an email is sent to the recievers set in the script.
-A flatfile and JSON is used to store wether the site was down last time the
-check was done so as to avoid spamming the email recievers.
+A flatfile and JSON is used to store whether the site was down last time the
+check was done so as to avoid spamming the email receivers.
 Run this as a cron job at regular intervals.
 
 Written quickly and dirty AKA functionally.
